71f1c1866a68cb72924a7608a7e0ef9055cf836c
Ryszard Mikulec <mikurys@gmail.com>
2016-01-17 11:10:47 +0100

    detector work but only in favorable conditions

--- data_matrix_detector.py
+++ data_matrix_detector.py
@@ -2,6 +2,10 @@ import numpy as np
 import cv2
 from data_matrix_crypto import DataMatrixCrypto
 
+"""data_matrix_detector.py: Can detext and read data matrix but not always.It would be good to improwe detect_matrix method.
+
+__author__ = "Ryszard Mikulec" """
+
 
 class DataMatrixDetector:
     def __init__(self, video, size=6):
@@ -27,7 +31,7 @@ class DataMatrixDetector:
     def check_matrix(self, matrix):
         for i in range(self.size):
             if (matrix[0][i] + matrix[-1][i] == i % 2 or matrix[0][i] + matrix[-1][i] == (i + 1) % 2) and (
-                        matrix[i][0] + matrix[i][-1] == i % 2 or matrix[i][0] + matrix[i][-1] == (i + 1) % 2):
+                                matrix[i][0] + matrix[i][-1] == i % 2 or matrix[i][0] + matrix[i][-1] == (i + 1) % 2):
                 pass
             else:
                 return False
@@ -39,7 +43,7 @@ class DataMatrixDetector:
 
     def turn_matrix(self, matrix):
         if matrix[0][0] == 1:
-            tab = [[(matrix[-j-1][i] + 1) % 2 for j in range(self.size)] for i in range(self.size)]
+            tab = [[(matrix[-j - 1][i] + 1) % 2 for j in range(self.size)] for i in range(self.size)]
         elif matrix[0][1] == 1:
             tab = [[(matrix[i][j] + 1) % 2 for j in range(self.size)] for i in range(self.size)]
         elif matrix[1][0] == 1:
@@ -56,7 +60,8 @@ class DataMatrixDetector:
         vy = (rect[3][1] - rect[0][1]) / self.size
         first = [rect[0][0] + (wx + vx) / 2, rect[0][1] + (wy + vy) / 2]
         try:
-            matrix = [[self.th[first[1] + i * wy + j * vy][first[0] + i * wx + j * vx] for i in range(self.size)] for j in range(self.size)]
+            matrix = [[self.th[first[1] + i * wy + j * vy][first[0] + i * wx + j * vx] for i in range(self.size)] for j
+                      in range(self.size)]
         except IndexError:
             return None
         if self.check_matrix(matrix):
@@ -72,22 +77,25 @@ class DataMatrixDetector:
         im2, contours, hierarchy = cv2.findContours(self.th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         return [contour for contour in contours if 3000 < cv2.contourArea(contour) < 130000]
 
+    def detect_matrix(self):
+        contours = self.take_contours()
+        counter = 0
+        for cnt, cnt2 in zip(contours, self.contours):
+            val = cv2.matchShapes(cnt, cnt2, 1, 0.0)
+            if val < 0.2:
+                rect = cv2.minAreaRect(cnt)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                cv2.drawContours(self.frame, [box], 0, (0, 0, 255), 2)
+                cv2.drawContours(self.frame, cnt, -1, (255, 0, 0), 3)
+                counter += 1
+        if counter == 1:
+            self.read_matrix(box)
+
     def run(self):
         while True:
             if self.take_frame():
-                contours = self.take_contours()
-                counter = 0
-                for cnt, cnt2 in zip(contours, self.contours):
-                    val = cv2.matchShapes(cnt, cnt2, 1, 0.0)
-                    if val < 0.2:
-                        rect = cv2.minAreaRect(cnt)
-                        box = cv2.boxPoints(rect)
-                        box = np.int0(box)
-                        cv2.drawContours(self.frame, [box], 0, (0, 0, 255), 2)
-                        cv2.drawContours(self.frame, cnt, -1, (255, 0, 0), 3)
-                        counter += 1
-                if counter == 1:
-                    self.read_matrix(box)
+                self.detect_matrix()
                 cv2.imshow("okno", self.frame)
                 cv2.waitKey(40)
             else:
