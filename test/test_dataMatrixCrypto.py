c99e4ea30a55d4d1dc534b7f4c553e3df9673622
Konixxx <MrozowskiKonrad@gmail.com>
2016-02-16 15:23:38 +0100

    DataMatrixCrypto test

--- test/test_dataMatrixCrypto.py
+++ test/test_dataMatrixCrypto.py
@@ -1,9 +1,21 @@
 from unittest import TestCase
-
+from data_matrix_crypto import DataMatrixCrypto
+# temp tests:
+# print(DataMatrix.encode(2678));
+# print(DataMatrix.decode()); #683
 
 class TestDataMatrixCrypto(TestCase):
     def test_encode(self):
-        self.fail()
+        expect = [[1, 0, 1, 0, 1, 0],
+                  [1, 0, 0, 0, 0, 1],
+                  [1, 1, 0, 1, 0, 0],
+                  [1, 0, 1, 1, 1, 1],
+                  [1, 0, 1, 1, 0, 0],
+                  [1, 1, 1, 1, 1, 1]]
+        arr = DataMatrixCrypto.encode(2678)
+        self.assertTrue(arr == expect)
 
     def test_decode(self):
-        self.fail()
+        arr = [[1,0,1,0,1,0],[1,0,0,0,0,1],[1,0,0,1,0,0],[1,1,0,1,0,1],[1,1,0,1,1,0],[1,1,1,1,1,1]]
+        expect = 683
+        self.assertTrue(DataMatrixCrypto.decode(arr) == expect)
