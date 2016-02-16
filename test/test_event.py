4d2d3e3a5e4282e4f4d414c56fb1bd52fe676f19
Konixxx <MrozowskiKonrad@gmail.com>
2016-02-16 15:18:31 +0100

    Event Test

--- test/test_event.py
+++ test/test_event.py
@@ -1,15 +1,29 @@
 from unittest import TestCase
+from camera_handler import Event
 
+
+def method():
+    return 1
+
+def method2():
+    return 2
 
 class TestEvent(TestCase):
     def test_add(self):
-        self.fail()
+        test = Event()
+        test += method
+        test += method2
+        test += method2
+        self.assertTrue(test.get_count() == 2)
 
     def test_remove(self):
-        self.fail()
+        test = Event()
+        self.assertTrue(test.get_count() == 0)
+        test += method2
+        test += method
+        self.assertTrue(test.get_count() == 2)
+        test -= method
+        self.assertTrue(test.get_count() == 1)
 
-    def test_trigger(self):
-        self.fail()
 
-    def test_get_count(self):
-        self.fail()
+
