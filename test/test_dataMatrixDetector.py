275f87201a8fc1bed8727a1389a02f09cb0c47eb
Konixxx <MrozowskiKonrad@gmail.com>
2016-02-18 17:51:57 +0100

    Update

--- test/test_dataMatrixDetector.py
+++ test/test_dataMatrixDetector.py
@@ -7,35 +7,69 @@ import cv2
 from unittest import TestCase
 import unittest
 from data_matrix_detector import DataMatrixDetector
+from db_connection import DBConnection
 
 class TestDataMatrixDetector(TestCase):
     def test_set_template(self):
         db = DBConnection("../data_base/test_base.db")
         det = DataMatrixDetector(db)
         det.set_template("../data_matrixes/template.jpg")
-
-        pass
+        self.assertGreater(len(det.contours),0)
 
     def test_check_matrix(self):
-        self.fail()
+        expect = [[1, 0, 1, 0, 1, 0],
+                  [1, 0, 0, 0, 0, 1],
+                  [1, 1, 0, 1, 0, 0],
+                  [1, 0, 1, 1, 1, 1],
+                  [1, 0, 1, 1, 0, 0],
+                  [1, 1, 1, 1, 1, 1]]
+        #rotated to the right
+        test = [[1,1,1,1,1,1],
+                [0,0,1,0,0,1],
+                [1,0,0,1,1,1],
+                [0,0,1,1,1,1],
+                [1,0,0,1,0,1],
+                [0,1,0,1,0,1]]
 
-    def test_turn_matrix(self):
-        self.fail()
-
-    def test_read_matrix(self):
-        self.fail()
-
-    def test_take_contours(self):
-        self.fail()
-
-    def test_detect_matrix(self):
-        #you should take a picture and name it test.jpg"
-        pass
-        capture = cv2.imread("test.jpg")
         db = DBConnection("../data_base/test_base.db")
         det = DataMatrixDetector(db)
         det.set_template("../data_matrixes/template.jpg")
-        capture = det.detect_matrix([capture])
+        self.assertTrue(det.check_matrix(expect))
+        self.assertFalse(det.check_matrix(test))
+
+    def test_turn_matrix(self):
+
+        expect = [[1, 0, 1, 0, 1, 0],
+                  [1, 0, 0, 0, 0, 1],
+                  [1, 1, 0, 1, 0, 0],
+                  [1, 0, 1, 1, 1, 1],
+                  [1, 0, 1, 1, 0, 0],
+                  [1, 1, 1, 1, 1, 1]]
+        #rotated to the right
+        test = [[1,1,1,1,1,1],
+                [0,0,1,0,0,1],
+                [1,0,0,1,1,1],
+                [0,0,1,1,1,1],
+                [1,0,0,1,0,1],
+                [0,1,0,1,0,1]]
+
+        db = DBConnection("../data_base/test_base.db")
+        det = DataMatrixDetector(db)
+        det.set_template("../data_matrixes/template.jpg")
+        test = det.turn_matrix(test)
+        self.assertEqual(test,expect)
+
+
+    def test_detect_matrix(self):
+        #you should take a picture of a matrix and name it test.jpg"
+
+     #   capture = cv2.imread("TestImages/test.jpg")
+     #   db = DBConnection("../data_base/test_base.db")
+     #   det = DataMatrixDetector(db)
+     #   det.set_template("../data_matrixes/template.jpg")
+     #   capture = det.detect_matrix([capture])
+     #   self.assertTrue(det.detected)
+        pass
 
 if __name__ == "__main__":
     unittest.main()
